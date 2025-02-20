add37644ffa5239974709edb707913226b73e66e
Minjo Park <consistent.312@gmail.com>
2025-02-20 17:45:03 +0900

    feat: Header UI 구현 및 이동 경로 설정

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="px-24 py-3 text-white bg-black">
+      <ul className="flex justify-between">
+        <li className="font-semibold text-2xl">
+          <Link to={"/"}>MBTI</Link>
+        </li>
+        <li className="text-lg">
+          <Link to={"/login"}>로그인</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
