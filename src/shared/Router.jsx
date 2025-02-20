d0bcbe3220494c841a5ba1514b998eeb6bf72d32
Minjo Park <consistent.312@gmail.com>
2025-02-20 16:38:42 +0900

    fix: Router 경로 수정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Signup from "../pages/Signup";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
 import Profile from "../pages/Profile";
+import Signup from "../pages/Signup";
 import TestPage from "../pages/TestPage";
 import TestResultPage from "../pages/TestResultPage";
 
@@ -10,7 +12,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/Login" element={<Signup />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/test" element={<TestPage />} />
         <Route path="/test-result" element={<TestResultPage />} />
