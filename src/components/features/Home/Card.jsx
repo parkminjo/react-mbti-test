d04c23bfb317cf049d51dd05149daa18afaf34b1
Minjo Park <consistent.312@gmail.com>
2025-02-20 22:15:29 +0900

    design: width값 삭제

--- src/components/features/Home/Card.jsx
+++ src/components/features/Home/Card.jsx
@@ -1,6 +1,6 @@
 const Card = ({ cardContent }) => {
   return (
-    <div className="text-left w-[400px] p-6 rounded-lg shadow-lg">
+    <div className="text-left p-6 rounded-lg shadow-lg">
       <h1 className="text-xl pb-4">{cardContent.title}</h1>
       <p className="text-gray-500">{cardContent.content}</p>
     </div>
