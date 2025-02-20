dd1fb55f232aa59bf04e34c179dbdc72c2643507
Minjo Park <consistent.312@gmail.com>
2025-02-20 19:09:57 +0900

    fix: screens 위치 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "465px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1640px",
+    },
   },
-  screens: {
-    sm: "465px",
-    md: "768px",
-    lg: "1024px",
-    xl: "1640px",
-  },
+
   plugins: [],
 };
