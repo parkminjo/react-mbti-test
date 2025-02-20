d381f0b397c011ecd8bdab47d4e9a325e02c6a7c
Minjo Park <consistent.312@gmail.com>
2025-02-20 21:34:07 +0900

    fix: tailwind style 변수로 분리하기

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
-import Card from "../components/features/Home/Card";
-import { cardContentList } from "../data/Home/cardContentList";
+import Card from "../components/features/home/Card";
+import { cardContentList } from "../data/home/cardContentList";
 
 const Home = () => {
   /** UI */
   return (
     <div className="w-screen text-center">
-      <h1 className="text-5xl font-semibold pb-5">무료 성격 테스트</h1>
-      <p className="text-xl text-gray-500 pb-8">
+      <h1 className={TitleStyle}>무료 성격 테스트</h1>
+      <p className={ContentStyle}>
         자신의 성격 유형을 확인할 수 있도록 솔직하게 답변해 주세요.
       </p>
       <div className="grid grid-cols-3 gap-2 mb-10">
@@ -27,3 +27,7 @@ export default Home;
 /** Tailwind Style */
 const buttonStyle =
   "w-[200px] rounded-full p-3 text-white bg-blue-500 hover:bg-blue-600";
+
+const TitleStyle = "text-5xl font-semibold pb-5";
+
+const ContentStyle = "text-xl text-gray-500 pb-8";
