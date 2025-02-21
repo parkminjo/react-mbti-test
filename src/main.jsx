5ed183a3131fa87f411375ec131d45810a6bda31
Minjo Park <consistent.312@gmail.com>
2025-02-21 16:40:50 +0900

    feat: queryClient 추가

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ToastContainer />
-    <App />
+    <QueryClientProvider>
+      <ToastContainer />
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
