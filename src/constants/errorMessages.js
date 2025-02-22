e0c0b4f455887cf6838f48c990988ac61608bcaf
Minjo Park <consistent.312@gmail.com>
2025-02-22 13:12:46 +0900

    chore: 에러 메시지 추가

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -5,5 +5,5 @@ export const ERROR_MESSAGES = {
   NICKNAME_REQUIRED_MESSAGE: "닉네임을 입력해주세요.",
   PASSWORD_CHECK: "비밀번호가 일치하지 않습니다.",
   LOGIN_CHECK: "로그인이 필요합니다.",
-  TESTRESULTS: "테스트 결과를 가져오는데 문제가 발생하였습니다.",
+  TEST_RESULTS: "테스트 결과를 가져오는데 문제가 발생하였습니다.",
 };
