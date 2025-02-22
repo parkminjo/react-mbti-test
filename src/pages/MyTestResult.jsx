e6f3c1e48c7f4d8e35a5189c66b4eb6aeaf50e2a
Minjo Park <consistent.312@gmail.com>
2025-02-22 12:01:30 +0900

    comment: 주석 추가

--- src/pages/MyTestResult.jsx
+++ src/pages/MyTestResult.jsx
@@ -5,13 +5,14 @@ const MyTestResult = () => {
   const [searchParams] = useSearchParams();
   const mbti = searchParams.get("mbti");
 
-  const mbtiDescriptionArr = mbtiDescriptions[mbti].split(":");
+  /** 해당 MBTI 설명 */
+  const myMbtiDescription = mbtiDescriptions[mbti].split(":");
 
   return (
     <div>
       <div className="w-[380px] flex flex-col gap-5">
-        <h1 className="text-2xl font-semibold">{`당신은 ${mbtiDescriptionArr[0]}입니다.`}</h1>
-        <p className="text-lg/8">{mbtiDescriptionArr[1]}</p>
+        <h1 className="text-2xl font-semibold">{`당신은 ${myMbtiDescription[0]}입니다.`}</h1>
+        <p className="text-lg/8">{myMbtiDescription[1]}</p>
         <Link to={"/all-test-results"}>
           <button className="w-[380px] h-[50px] rounded-lg text-white bg-blue-500 transition delay-100 duration-200 ease-in-out hover:bg-blue-600">
             다른 사람 결과 보러 가기
