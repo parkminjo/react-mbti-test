17b4ab600000f76890a7528ff877f2051c52b10e
Minjo Park <consistent.312@gmail.com>
2025-02-24 14:20:12 +0900

    refactor: 공통 폰트 설정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Card from "../components/features/home/Card";
 import { ERROR_MESSAGES } from "../constants/errorMessages";
 import { cardContentList } from "../data/home/cardContentList";
 import useAuthStore from "../zustand/authStore";
+import { TwText } from "../styles/TwTextStyle";
 
 const Home = () => {
   const { isLogin } = useAuthStore((state) => state);
@@ -16,11 +17,11 @@ const Home = () => {
 
   return (
     <div className="w-full md:w-1024 flex flex-col justify-center items-center">
-      <h1 className={TitleStyle}>무료 성격 테스트</h1>
-      <p className={ContentStyle}>
+      <h1 className={TwText.titleStyle}>무료 성격 테스트</h1>
+      <p className={TwText.contentStyle}>
         자신의 성격 유형을 확인할 수 있도록 솔직하게 답변해 주세요.
       </p>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-10">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 my-6">
         {cardContentList.map((cardContent) => {
           return <Card key={cardContent.id} cardContent={cardContent} />;
         })}
@@ -39,7 +40,3 @@ export default Home;
 /** Tailwind Style */
 const buttonStyle =
   "w-[200px] rounded-full p-3 text-white bg-blue-500 transition delay-100 duration-200 ease-in-out hover:bg-blue-600";
-
-const TitleStyle = "text-5xl font-semibold mb-5";
-
-const ContentStyle = "text-xl text-gray-500 mb-8";
--- src/styles/TwTextStyle.js
+++ src/styles/TwTextStyle.js
@@ -1,4 +1,5 @@
 export const TwText = {
   titleStyle: "text-3xl font-semibold mb-5",
   subTitleStyle: "text-2xl font-semibold",
+  contentStyle: "text-xl text-gray-500",
 };
