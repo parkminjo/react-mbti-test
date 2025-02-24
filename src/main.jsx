bbca067fc617364467c9e0e06ff59c4db92da849
Minjo Park <consistent.312@gmail.com>
2025-02-24 14:20:29 +0900

    chore: toast 알림 설정 추가

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ToastContainer />
+      <ToastContainer autoClose={1000} />
       <App />
     </QueryClientProvider>
   </StrictMode>
