194322b5e330c089d4c142d73c7a397b569101da
Minjo Park <consistent.312@gmail.com>
2025-02-24 17:50:31 +0900

    design: 카카오톡 공유 이미지 수정

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -6,4 +6,4 @@ export const KOREAN = "ko-KR";
 
 export const TEST_CHECK_MESSAGE = "테스트를 진행해주세요.";
 
-export const KAKAO_IMAGE_URL = "https://ifh.cc/g/YkWFfR.png";
+export const KAKAO_IMAGE_URL = "https://ifh.cc/g/3RxcT9.png";
