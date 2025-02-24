ee28669fe34405d8ec70b8677fdeb2ac7572ed63
Minjo Park <consistent.312@gmail.com>
2025-02-24 23:35:17 +0900

    design: margin bottom 값 수정

--- src/pages/AllTestResultPage.jsx
+++ src/pages/AllTestResultPage.jsx
@@ -3,7 +3,7 @@ import { TwText } from "../styles/TwTextStyle";
 
 const AllTestResultPage = () => {
   return (
-    <div className="w-full flex flex-col items-center mb-20">
+    <div className="w-full flex flex-col items-center mb-5 sm:mb-20">
       <h1 className={TwText.titleStyle}>모든 MBTI 테스트 결과</h1>
       <TestResultList />
     </div>
