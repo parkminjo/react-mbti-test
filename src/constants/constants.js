5310408f69d7688dc75f5e218c4ad2e04aef01b6
Minjo Park <consistent.312@gmail.com>
2025-02-25 14:18:35 +0900

    fix: stale time 추가

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -9,3 +9,5 @@ export const TEST_CHECK_MESSAGE = "테스트를 진행해주세요.";
 export const KAKAO_IMG_URL = "https://ifh.cc/g/3RxcT9.png";
 
 export const SPINNER_IMG = "https://ifh.cc/g/Y0rdgo.gif";
+
+export const STALE_TIME = 60 * 1000;
--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,15 @@ import { ToastContainer } from "react-toastify";
 import App from "./App.jsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { STALE_TIME } from "./constants/constants.js";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: STALE_TIME,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
