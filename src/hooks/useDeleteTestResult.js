ccf94dd5a0c1f3b3ef72019c638281e6ca10c8d3
Minjo Park <consistent.312@gmail.com>
2025-02-25 15:02:38 +0900

    feat: optimistic updates 추가

--- src/hooks/useDeleteTestResult.js
+++ src/hooks/useDeleteTestResult.js
@@ -7,10 +7,21 @@ export const useDeleteTestResult = () => {
 
   return useMutation({
     mutationFn: deleteTestResult,
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: [QUERY_KEY],
-      });
+    onMutate: async (testResultId) => {
+      await queryClient.cancelQueries({ queryKey: [QUERY_KEY] });
+
+      const prevTestResults = queryClient.getQueryData([QUERY_KEY]);
+
+      queryClient.setQueryData([QUERY_KEY], (old) =>
+        old.filter((test) => test.id !== testResultId)
+      );
+      return { prevTestResults };
+    },
+    onError: (_, __, context) => {
+      queryClient.setQueryData([QUERY_KEY], context.prevTestResults);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY] });
     },
   });
 };
