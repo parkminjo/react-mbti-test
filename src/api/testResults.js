70be95db79afb10f4da9dd8d51e8549885b08032
Minjo Park <consistent.312@gmail.com>
2025-02-25 10:14:02 +0900

    refactor: 에러 메시지 추가

--- src/api/testResults.js
+++ src/api/testResults.js
@@ -3,35 +3,53 @@ import { JSON_END_POINT } from "../constants/constants";
 import { ERROR_MESSAGES } from "../constants/errorMessages";
 import { jsonAPI } from "./axiosInstance";
 
+/**
+ * 테스트 결과 조회하기
+ * @returns testResults
+ */
 export const getTestResults = async () => {
   try {
-    const response = await jsonAPI.get("/testResults");
-    return response.data;
+    const { data: testResults } = await jsonAPI.get("/testResults");
+    return testResults;
   } catch (error) {
     toast.error(ERROR_MESSAGES.TEST_RESULTS, error);
   }
 };
 
+/**
+ * 테스트 결과 추가하기
+ * @param {*} resultData
+ */
 export const createTestResult = async (resultData) => {
   try {
     await jsonAPI.post(JSON_END_POINT, resultData);
   } catch (error) {
-    console.log(error);
+    toast.error(ERROR_MESSAGES.TEST_RESULTS_ADD, error);
   }
 };
 
-export const deleteTestResult = async (id) => {
+/**
+ * 테스트 결과 삭제하기
+ * @param {*} testId
+ */
+export const deleteTestResult = async (testId) => {
   try {
-    await jsonAPI.delete(JSON_END_POINT + `/${id}`);
+    await jsonAPI.delete(JSON_END_POINT + `/${testId}`);
   } catch (error) {
-    console.log(error);
+    toast.error(ERROR_MESSAGES.TEST_RESULTS_DELETE, error);
   }
 };
 
-export const updateTestResultVisibility = async ({ id, visibility }) => {
+/**
+ * 테스트 결과 공개 여부 변경
+ * @param {*} testId, visibility
+ */
+export const updateTestResultVisibility = async ({ testId, visibility }) => {
   try {
-    await jsonAPI.patch(JSON_END_POINT + `/${id}`, { visibility: !visibility });
+    await jsonAPI.patch(JSON_END_POINT + `/${testId}`, {
+      visibility: !visibility,
+    });
   } catch (error) {
-    console.log(error);
+    toast.error(ERROR_MESSAGES.TEST_RESULTS_UPDATE, error);
   }
 };
--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -6,4 +6,7 @@ export const ERROR_MESSAGES = {
   PASSWORD_CHECK: "비밀번호가 일치하지 않습니다.",
   LOGIN_CHECK: "로그인이 필요합니다.",
   TEST_RESULTS: "테스트 결과를 가져오는데 문제가 발생하였습니다.",
+  TEST_RESULTS_ADD: "테스트 결과를 추가하는데 문제가 발생하였습니다.",
+  TEST_RESULTS_DELETE: "테스트 결과를 삭제하는데 문제가 발생하였습니다.",
+  TEST_RESULTS_UPDATE: "공개 여부를 변경하는데 문제가 발생하였습니다.",
 };
