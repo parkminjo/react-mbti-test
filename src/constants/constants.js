59fbb3575524e260931bb49b606b00418e49ddc5
Minjo Park <consistent.312@gmail.com>
2025-02-25 13:01:16 +0900

    fix: 이미지 주소로 변경

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -8,4 +8,4 @@ export const TEST_CHECK_MESSAGE = "테스트를 진행해주세요.";
 
 export const KAKAO_IMAGE_URL = "https://ifh.cc/g/3RxcT9.png";
 
-export const SPINNER_IMG = "/src/assets/spinner.gif";
+export const SPINNER_IMG = "https://ifh.cc/g/Y0rdgo.gif";
